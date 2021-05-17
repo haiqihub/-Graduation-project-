18ba84f3437529770beac09752300ca400040c68
haiqihub <2322894543@qq.com>
2021-05-17 14:13:16 +0800

    detail

--- 相册排版界面/Start.Designer.cs
+++ 相册排版界面/Start.Designer.cs
@@ -63,7 +63,7 @@
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Location = new System.Drawing.Point(-5, 96);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(729, 207);
             this.pictureBox1.TabIndex = 3;
@@ -81,7 +81,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Name = "Start";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Start";
             this.Load += new System.EventHandler(this.Start_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
